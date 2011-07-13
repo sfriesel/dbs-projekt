980169ac36648be9de39d1dbe4c8a5c39cf011c1
Alexa Schlegel <alexa.schlegel@gmail.com>
2011-07-14 01:50:01 +0200

    Added some more caches

--- src/datahandling/Cache.java
+++ src/datahandling/Cache.java
@@ -2,20 +2,27 @@ package datahandling;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 public class Cache {
 
 	private static Cache instance = null;
 
-	public ArrayList<String> movie = null;
-	public ArrayList<String> actor = null;
-	public ArrayList<String> starring = null;
+	public HashSet<String> movie = null;
+	public HashSet<String> actor = null;
+	public HashSet<String> customer = null;
+	public HashSet<String> director = null;
+	public HashSet<String> directedBy = null;
+	public HashSet<String> location = null;
 
 	private Cache() {
-		movie = new ArrayList<String>();
-		actor = new ArrayList<String>();
-		starring = new ArrayList<String>();
+		movie = new HashSet<String>();
+		actor = new HashSet<String>();
+		customer = new HashSet<String>();
+		director = new HashSet<String>();
+		directedBy = new HashSet<String>();
+		location = new HashSet<String>();
 	}
 
 	public static Cache getInstance() {
