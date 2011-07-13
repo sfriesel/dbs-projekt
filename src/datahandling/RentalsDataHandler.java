41cc660a6d9028451439cca2173d80916cb77362
Alexa Schlegel <alexa.schlegel@gmail.com>
2011-07-13 23:26:54 +0200

    some changes

--- src/datahandling/RentalsDataHandler.java
+++ src/datahandling/RentalsDataHandler.java
@@ -1,18 +1,20 @@
 package datahandling;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 
 import database.DBConnector;
 
 public class RentalsDataHandler extends AbstractDataHandler {
-	
-	private PreparedStatement selectCustomerIDStmt = null;
+
 	private PreparedStatement selectMovStmt = null;
-	//private PreparedStatement insert
-	
+	private PreparedStatement insertRentalStmt = null;
+
 	static private DBConnector con;
-	
+
 	public RentalsDataHandler() {
 		super("Daten/rentals.list", 1, 1149, "\t+");
 
@@ -21,24 +23,39 @@ public class RentalsDataHandler extends AbstractDataHandler {
 	}
 
 	@Override
-	protected void insertDB(String[] arrayLine) {
-		// TODO Auto-generated method stub
+	protected void insertDB(String[] arrayLine) throws SQLException {
 
+		int id = Integer.parseInt(arrayLine[0]);
+		String type = arrayLine[1];
+		String title = arrayLine[2];
+		Timestamp start = DataHandlerUtils.stringToTimestamp(arrayLine[3]);
+		int duration = Integer.parseInt(arrayLine[4]);
+		
+		selectMovStmt.setString(1, title);
+		ResultSet movRS = selectMovStmt.executeQuery();
+		
+		// only if the movie is in our DB add retal to DB
+		if (movRS.next()){
+			insertRentalStmt.setInt(1, id);
+			insertRentalStmt.setString(2, type);
+			insertRentalStmt.setString(3, title);
+			insertRentalStmt.setTimestamp(4, start);
+			insertRentalStmt.setInt(5, duration);
+			insertRentalStmt.execute();
+		}
+	
 	}
-
-
 
 	@Override
 	protected void closeStatements() throws SQLException {
-		// TODO Auto-generated method stub
-		
+		insertRentalStmt.close();
 	}
-
-
 
 	@Override
 	protected void prepareStatements() throws SQLException {
-		// TODO Auto-generated method stub
-		
+		insertRentalStmt = con.connection
+				.prepareStatement("INSERT INTO Rental (customer, priceModel, movie, start, duration) VALUES (?,?,?,?,?);");
+	
+		selectMovStmt = con.connection.prepareStatement("SELECT * FROM Movie WHERE title = ?;");
 	}
 }
--- src/testing/TestDataHandlers.java
+++ src/testing/TestDataHandlers.java
@@ -7,70 +7,112 @@ import database.DBConnector;
 import datahandling.AbstractDataHandler;
 import datahandling.ActorsActressDataHandler;
 import datahandling.CustomerDataHandler;
+import datahandling.DirectorsDataHandler;
 import datahandling.LocationDataHandler;
 import datahandling.MovieDataHandler;
 import datahandling.ReleaseDataHandler;
+import datahandling.RentalsDataHandler;
 
 public class TestDataHandlers {
 	public static void main(String[] args) {
-		
-		DBConnector.configure(
-				"localhost", "5432", "movies", "alexa", "dinkel");
 
-		// AbstractDataHandler handler1 = new CustomerDataHandler();
-		// handler1.parse();
+		DBConnector.configure("localhost", "5432", "movies", "alexa", "dinkel");
 
-//		AbstractDataHandler handler2 = new MovieDataHandler();
+//		AbstractDataHandler handler1 = new CustomerDataHandler();
 //		try {
-//			handler2.parse();
+//			System.out.println("Start Customer.");
+//			handler1.parse();
+//			System.out.println("End Customer.");
 //		} catch (IOException e1) {
-//			// TODO Auto-generated catch block
 //			e1.printStackTrace();
 //		} catch (SQLException e1) {
-//			// TODO Auto-generated catch block
+//			System.out.println(handler1.counter);
 //			e1.printStackTrace();
 //		}
 //
-//		 AbstractDataHandler handler3 = new ReleaseDataHandler();
-//		 try {
-//		 handler3.parse();
-//		 } catch (IOException e) {
-//		 // TODO Auto-generated catch block
-//		 e.printStackTrace();
-//		 } catch (SQLException e) {
-//		 // TODO Auto-generated catch block
-//		 e.printStackTrace();
-//		 }
-
-//		AbstractDataHandler handler4 = new LocationDataHandler();
+//		AbstractDataHandler handler2 = new MovieDataHandler();
 //		try {
-//			handler4.parse();
+//			System.out.println("Start Movie.");
+//			handler2.parse();
+//			System.out.println("End Movie.");
+//		} catch (IOException e1) {
+//			e1.printStackTrace();
+//		} catch (SQLException e1) {
+//			System.out.println(handler2.counter);
+//			e1.printStackTrace();
+//		}
+//
+//		AbstractDataHandler handler3 = new ReleaseDataHandler();
+//		try {
+//			System.out.println("Start Release.");
+//			handler3.parse();
+//			System.out.println("End Release.");
 //		} catch (IOException e) {
-//			// TODO Auto-generated catch block
 //			e.printStackTrace();
 //		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
+//			System.out.println(handler3.counter);
 //			e.printStackTrace();
 //		}
-		
-		AbstractDataHandler handler5 = new ActorsActressDataHandler("Daten/actors.list", 239 , 11272388, "\t+", "m");
+//
+//		AbstractDataHandler handler4 = new LocationDataHandler();
+//		try {
+//			System.out.println("Start Location.");
+//			handler4.parse();
+//			System.out.println("End Location.");
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		} catch (SQLException e) {
+//			System.out.println(handler4.counter);
+//			e.printStackTrace();
+//		}
+//
+//		AbstractDataHandler handler5 = new ActorsActressDataHandler(
+//				"Daten/actors.list", 239, 11272388, "\t+", "m");
+//		try {
+//			System.out.println("Start Actor.");
+//			handler5.parse();
+//			System.out.println("End Actor.");
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		} catch (SQLException e) {
+//			System.out.println(handler5.counter);
+//			e.printStackTrace();
+//		}
+
+		AbstractDataHandler handler6 = new ActorsActressDataHandler(
+				"Daten/actresses.list", 241, 6592288, "\t+", "f");
 		try {
-			handler5.parse();
+			System.out.println("Start Actress.");
+			handler6.parse();
+			System.out.println("End Actress.");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (SQLException e) {
+			System.out.println(handler6.counter);
 			e.printStackTrace();
 		}
-		
-		AbstractDataHandler handler6 = new ActorsActressDataHandler("Daten/actress.list", 241, 6592288, "\t+", "w");
+
+		AbstractDataHandler handler7 = new DirectorsDataHandler();
 		try {
-			handler6.parse();
+			System.out.println("Start Directors.");
+			handler7.parse();
+			System.out.println("End Directors.");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			System.out.println(handler7.counter);
+			e.printStackTrace();
+		}
+
+		AbstractDataHandler handler8 = new RentalsDataHandler();
+		try {
+			System.out.println("Start Rentals.");
+			handler8.parse();
+			System.out.println("End Rentals.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println(handler8.counter);
 			e.printStackTrace();
 		}
 	}
