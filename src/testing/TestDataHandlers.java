b83c01d3b32f573c9d6261fda115795ee1fb4b6f
alexa <alexa@creature.(none)>
2011-07-14 23:04:30 +0200

    Just added some SQLException output

--- src/testing/TestDataHandlers.java
+++ src/testing/TestDataHandlers.java
@@ -7,7 +7,7 @@ import database.DBConnector;
 public class TestDataHandlers {
 	public static void main(String[] args) {
 		
-		DBConnector.configure("localhost", "5432", "movies", "csw", "csw");
+		DBConnector.configure("localhost", "5432", "movies", "alexa", "dinkel");
 		DBConnector con = DBConnector.getInstance();
 		try {
 			con.resetDB();
@@ -15,6 +15,7 @@ public class TestDataHandlers {
 			e2.printStackTrace();
 		} catch (SQLException e2) {
 			e2.printStackTrace();
+			e2.getNextException().printStackTrace();
 		}
 	}
 }
