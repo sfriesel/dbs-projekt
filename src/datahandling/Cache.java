fcab89ada5dec782f44f0030e136c49a3c14eb56
alexa <alexa@creature.(none)>
2011-07-14 23:02:02 +0200

    Only the caches, which are really needed are is the Cache Class

--- src/datahandling/Cache.java
+++ src/datahandling/Cache.java
@@ -5,23 +5,12 @@ import java.util.HashSet;
 public class Cache {
 
 	private static Cache instance = null;
-
 	public HashSet<String> movie = null;
-	public HashSet<String> actor = null;
 	public HashSet<String> customer = null;
-	public HashSet<String> director = null;
-	public HashSet<String> directedBy = null;
-	public HashSet<String> location = null;
-	public HashSet<String> shotIn = null;
 
 	private Cache() {
-		movie = new HashSet<String>();
-		actor = new HashSet<String>();
-		customer = new HashSet<String>();
-		director = new HashSet<String>();
-		directedBy = new HashSet<String>();
-		location = new HashSet<String>();
-		shotIn = new HashSet<String>();
+		this.movie = new HashSet<String>();
+		this.customer = new HashSet<String>();
 	}
 
 	public static Cache getInstance() {
