11c7682748ec413734350776b3144a3ba4adb505
alexa <alexa@creature.(none)>
2011-07-15 02:19:03 +0200

    Changed the cache for customers to Integer values, so now are also customers AND rentals in the DB

--- src/datahandling/Cache.java
+++ src/datahandling/Cache.java
@@ -6,11 +6,11 @@ public class Cache {
 
 	private static Cache instance = null;
 	public HashSet<String> movie = null;
-	public HashSet<String> customer = null;
+	public HashSet<Integer> customer = null;
 
 	private Cache() {
 		this.movie = new HashSet<String>();
-		this.customer = new HashSet<String>();
+		this.customer = new HashSet<Integer>();
 	}
 
 	public static Cache getInstance() {
