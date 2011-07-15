97b8bfba06033eb28b4ba6043ecd3dbddec8ffbd
Alexa Schlegel <alexa.schlegel@gmail.com>
2011-07-16 01:23:03 +0200

    Schon mal die Aufgaben b,c,d hinzugefügt, sind aber noch leer

---
+++ src/sqlqueries/PlainSQL1A.java
@@ -0,0 +1,34 @@
+package sqlqueries;
+
+import java.sql.*;
+
+import cli.PrintResult;
+
+import cli.MenuEntry;
+import database.*;
+
+public class PlainSQL1A implements MenuEntry {
+
+	@Override
+	public String getName() {
+		return "Verteilung der Preismodelle";
+	}
+
+	@Override
+	public String getDiscription() {
+		return "Wie viele Kunden haben sich für welches Preismodell entschieden?";
+	}
+
+	public void execute() throws SQLException {
+		DBConnector con = DBConnector.getInstance();
+		
+		Statement stmt = con.connection.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT pricemodel, count( DISTINCT customer) "
+				+ "FROM rental " + "GROUP BY pricemodel;");
+
+		PrintResult pr = new PrintResult(rs);
+		pr.setHead("Presimodell", "Anzahl der Kunden");
+		pr.setDescription("Übersich über die Verteilung der Preismodelle:");
+		pr.print();
+	}
+}
---
+++ src/sqlqueries/PlainSQL1B.java
@@ -0,0 +1,25 @@
+package sqlqueries;
+
+import cli.MenuEntry;
+
+public class PlainSQL1B implements MenuEntry {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDiscription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void execute() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
