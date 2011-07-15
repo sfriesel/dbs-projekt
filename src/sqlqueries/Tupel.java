382a51dfa3ddd02b3ed81b86305e8d629314bd40
Alexa Schlegel <alexa.schlegel@gmail.com>
2011-07-15 20:30:10 +0200

    added a equal and hashcode function

--- src/sqlqueries/Tupel.java
+++ src/sqlqueries/Tupel.java
@@ -8,4 +8,20 @@ public class Tupel {
 		this.name = name;
 		this.gender = gender;
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Tupel t = (Tupel) obj;
+		return (name.equals(t.name) && gender.equals(t.gender));
+	}
+
+	@Override
+	public String toString() {
+		return name + "(" + gender + ")";
+	}
+
+	@Override
+	public int hashCode() {
+		return name.hashCode() + gender.hashCode();
+	}
 }
