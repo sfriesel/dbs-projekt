d62af00a70a325d4a158abbea527b559845cbf3c
Alexa Schlegel <alexa.schlegel@gmail.com>
2011-07-16 00:08:07 +0200

    Ist ein MenuEntry und kuemmert sich um das befüllen der Datenbank, es liest auch das Schema ein.

---
+++ src/database/DBHandler.java
@@ -0,0 +1,63 @@
+package database;
+
+import cli.MenuEntry;
+import datahandling.AbstractDataHandler;
+import datahandling.ActorsActressDataHandler;
+import datahandling.CustomerDataHandler;
+import datahandling.DirectorsDataHandler;
+import datahandling.LocationDataHandler;
+import datahandling.MovieDataHandler;
+import datahandling.ReleaseDataHandler;
+import datahandling.RentalsDataHandler;
+
+public class DBHandler implements MenuEntry {
+
+	@Override
+	public String getName() {
+		return "Einlesen der DB";
+	}
+
+	@Override
+	public String getDiscription() {
+		return "Die Datenbank wird komplett neu gemacht.";
+	}
+
+	@Override
+	public void execute() throws Exception {
+		
+		System.out.println("Einlesen der Datenbank wird gestartet ...");
+
+		DBConnector con = DBConnector.getInstance();
+		con.executeSqlScript("Datenbankschema/scriptSQL");
+
+		AbstractDataHandler handler = new CustomerDataHandler();
+		handler.parse();
+
+		handler = new MovieDataHandler();
+		handler.parse();
+
+		handler = new ReleaseDataHandler();
+		handler.parse();
+
+		handler = new DirectorsDataHandler();
+		handler.parse();
+
+		handler = new LocationDataHandler();
+		handler.parse();
+
+		handler = new RentalsDataHandler();
+		handler.parse();
+
+		handler = new ActorsActressDataHandler("Daten/actors.list", 239,
+				11272388, "\t+", "m");
+		handler.parse();
+
+		handler = new ActorsActressDataHandler("Daten/actresses.list", 241,
+				6592286, "\t+", "f");
+		handler.parse();
+
+		// TODO parse plot
+
+		System.out.println(AbstractDataHandler.time / 1000f / 60f + " min");
+	}
+}
