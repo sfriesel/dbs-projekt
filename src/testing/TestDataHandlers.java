7b7ef01d0d6068606fe7c691108a1ed052822680
Alexa Schlegel <alexa.schlegel@gmail.com>
2011-07-16 00:12:05 +0200

    Brauche ich erstmal nicht mehr.

--- src/testing/TestDataHandlers.java
+++ src/testing/TestDataHandlers.java
@@ -1,21 +1,7 @@
 package testing;
 
-import java.io.IOException;
-import java.sql.SQLException;
-import database.DBConnector;
-
 public class TestDataHandlers {
 	public static void main(String[] args) {
-		
-		DBConnector.configure("localhost", "5432", "movies", "alexa", "dinkel");
-		DBConnector con = DBConnector.getInstance();
-		try {
-			con.resetDB();
-		} catch (IOException e2) {
-			e2.printStackTrace();
-		} catch (SQLException e2) {
-			e2.printStackTrace();
-			e2.getNextException().printStackTrace();
-		}
+
 	}
 }
