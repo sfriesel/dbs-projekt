2ae5a9f1f70ca4f3b81671882cb90abe1ea4c45f
Alexa Schlegel <alexa.schlegel@gmail.com>
2011-07-17 23:12:47 +0200

    Kunden und Movie hinzufügen ist fertig

--- src/inserts/InsertCustomer.java
+++ src/inserts/InsertCustomer.java
@@ -2,10 +2,9 @@ package inserts;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintStream;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import cli.MenuEntryInterface;
 import database.DBConnector;
@@ -24,14 +23,21 @@ public class InsertCustomer implements MenuEntryInterface {
 
 	@Override
 	public void execute() throws Exception {
+
+		DBConnector con = DBConnector.getInstance();
+		
+		PreparedStatement insertStmt = con.connection
+				.prepareStatement("INSERT INTO Customer (surname, forename, "
+						+ "street, streetnumber, zip, city, telephone) VALUES (?,?,?,?,?,?,?)");
+		
+		PreparedStatement selectStmt = con.connection
+				.prepareStatement("SELECT * FROM Customer "
+						+ "WHERE surname = ? AND forename = ?;");
+		
+
 		System.out.println("Geben Sie einen neuen Kunden ein:");
 		System.out
 				.println("<Nachname, Vorame, Straße, Hausnummer, PLZ, Stadt, Telefonnummer>");
-
-		DBConnector con = DBConnector.getInstance();
-		PreparedStatement insertStmt = con.connection
-				.prepareStatement("INSERT INTO Customer (surname, forename, "
-						+ "street, streetnumber, zip, city, telephone) VALUES (?,?,?,?,?,?,?,?)");
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String input = br.readLine();
@@ -43,20 +49,8 @@ public class InsertCustomer implements MenuEntryInterface {
 							+ "<Nachname, Vorame, Straße, Hausnummer, PLZ, Stadt, Telefonnummer>");
 			input = br.readLine();
 		}
-
+		
 		String[] data = input.split(", ");
-
-		insertStmt.setString(1, data[0]);
-		insertStmt.setString(2, data[1]);
-		insertStmt.setString(3, data[2]);
-		insertStmt.setInt(4, Integer.parseInt(data[3]));
-		insertStmt.setString(5, data[4]);
-		insertStmt.setString(6, data[5]);
-		insertStmt.setString(7, data[6]);
-
-		PreparedStatement selectStmt = con.connection
-				.prepareStatement("SELECT * FROM Customer "
-						+ "WHERE surname = ? AND forename = ?;");
 
 		selectStmt.setString(1, data[0]);
 		selectStmt.setString(2, data[1]);
@@ -64,13 +58,29 @@ public class InsertCustomer implements MenuEntryInterface {
 		ResultSet rs = selectStmt.executeQuery();
 
 		if (!rs.next()) {
-			insertStmt.execute();
-			System.out.println("Kunde " + data[0] + ", " + data[1]
-					+ "wurde hinzugefügt.");
+
+			insertStmt.setString(1, data[0]);
+			insertStmt.setString(2, data[1]);
+			insertStmt.setString(3, data[2]);
+			insertStmt.setString(4, data[3]);
+			insertStmt.setString(5, data[4]);
+			insertStmt.setString(6, data[5]);
+			insertStmt.setString(7, data[6]);
+			
+			try {
+				insertStmt.execute();
+				con.connection.commit();
+			} catch (SQLException e) {
+				e.printStackTrace();
+				e.getNextException().printStackTrace();
+			}
+			System.out.println("Kunde [" + data[0] + ", " + data[1]
+					+ "] wurde hinzugefügt.");
 		} else {
 			System.out
 					.println("Neuer Kunde konnte nicht hinzugefügt werden, Kunde mit gleichem Namen "
 							+ "existiert bereits.");
+			System.out.println();
 		}
 	}
 }
--- src/inserts/InsertMovie.java
+++ src/inserts/InsertMovie.java
@@ -1,6 +1,13 @@
 package inserts;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
+
 import cli.MenuEntryInterface;
+import database.DBConnector;
 
 public class InsertMovie implements MenuEntryInterface {
 
@@ -16,7 +23,55 @@ public class InsertMovie implements MenuEntryInterface {
 
 	@Override
 	public void execute() throws Exception {
-		// TODO Auto-generated method stub
-		
+
+		System.out.println("Geben Sie einen neuen Movie ein:");
+		System.out
+				.println("<Titel, [Beschreibung], Kategorie(A,B), [ReleaseDatum dd:mm:yyyy], [ReleasLand]>");
+
+		DBConnector con = DBConnector.getInstance();
+		PreparedStatement insertStmt = con.connection
+				.prepareStatement("INSERT INTO Movie (title, description, "
+						+ "category, release, rel_country) VALUES (?,?,?,?,?)");
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+
+		while (input.split(", ").length != 5) {
+
+			System.out
+					.println("Folgendes Format beachten: "
+							+ "<Titel, [Beschreibung], Kategorie(A,B), [ReleaseDatum dd:mm:yyyy], [ReleasLand]>");
+			input = br.readLine();
+		}
+
+		String[] data = input.split(", ");
+
+		insertStmt.setString(1, data[0]);
+		insertStmt.setString(2, data[1]);
+		insertStmt.setString(3, data[2]);
+
+		SimpleDateFormat format = new SimpleDateFormat("d:MM:yyyy");
+		java.util.Date date = format.parse(data[3]);
+		java.sql.Date sqlD = new java.sql.Date(date.getTime());
+
+		insertStmt.setDate(4, sqlD);
+		insertStmt.setString(5, data[4]);
+
+		PreparedStatement selectStmt = con.connection
+				.prepareStatement("SELECT * FROM Movie " + "WHERE title = ?;");
+
+		selectStmt.setString(1, data[0]);
+
+		ResultSet rs = selectStmt.executeQuery();
+
+		if (!rs.next()) {
+			insertStmt.execute();
+			con.connection.commit();
+			System.out.println("Movie [" + data[0] + "] wurde hinzugefügt.");
+		} else {
+			System.out
+					.println("Neuer Movie konnte nicht hinzugefügt werden, Movie mit gleichem Titel "
+							+ "existiert bereits.");
+		}
 	}
 }
