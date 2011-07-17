f8b6a34d91cb7896959526b5d73b689f9b2624fe
Stefan Friesel <stefan.friesel@googlemail.com>
2011-07-17 22:09:27 +0200

    use MainMenu

--- src/cli/Main.java
+++ src/cli/Main.java
@@ -7,21 +7,7 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class Main {
-
-	private static ArrayList<MenuEntryInterface> menu;
-
-	private static void printMenu() {
-		System.out.println("DVD-Verleih-Menü:");
-
-		for (int i = 0; i < menu.size(); i++) {
-			System.out.println("[" + i + "] " + menu.get(i).getName());
-		}
-		System.out.println();
-
-	}
-
 	public static void main(String[] args) throws Exception {
-
 		// Configure DB connection
 		if (args.length < 3) {
 			System.err
@@ -34,30 +20,6 @@ public class Main {
 			DBConnector.configure(null, null, args[0], args[1], args[2]);
 		}
 
-		// set up menueEntries
-		menu = new ArrayList<MenuEntryInterface>();
-		menu.add(new ExitEntry());
-		menu.add(new DBHandler());
-		menu.add(new PlainSQL1A());
-		menu.add(new PlainSQL1B());
-		menu.add(new PlainSQL1C());
-		menu.add(new PlainSQL1D());
-		menu.add(new AdvancedSQL2A());
-		menu.add(new AdvancedSQL2B());
-		menu.add(new AccountingPerYearCustomer());
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int input = -1;
-		do {
-			printMenu();
-
-			try {
-				input = Integer.parseInt(br.readLine());
-			} catch (NumberFormatException e) {
-			}
-
-			menu.get(input).execute();
-
-		} while (input != '0');
+		new MainMenu().execute();
 	}
 }
